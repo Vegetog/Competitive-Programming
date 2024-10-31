3b8663a414327dd7522409ee98d03850ba486696
Vegetog <2542856720@qq.com>
2024-10-31 22:04:42 +0800

    template update

--- _CP_template.cpp
+++ _CP_template.cpp
@@ -1,6 +1,7 @@
-#include <bits/stdc++.h>
 #ifndef ONLINE_JUDGE
 #include "_my_utils.h"
+#else
+#include <bits/stdc++.h>
 #endif
 #define int long long
 
@@ -10,7 +11,6 @@ using PII = std::pair<int, int>;
 
 constexpr ll INF = 2E18 + 10;
 constexpr int N = 2E6 + 10;
-// struct CMP{bool operator()(const int & x, const int &y) const{ return x<y; }};
 
 void SINGLE_TEST() 
 {
@@ -27,3 +27,27 @@ signed main() {
         SINGLE_TEST();
     }
 }
+/*---------------------------------------------------------------------------------
+struct CMP{bool operator()(const int & x, const int &y) const{ return x<y; }};
+
+template <class T>
+map<T, int> sortMap(const map<T, int> mp) {
+    vector<pair<T, int>> v(mp.begin(), mp.end());
+    sort(v.begin(), v.end(),
+         [&](auto x, auto y) { return x.second < y.second; });
+    map<T, int> ans;
+    for (auto [lo, v] : v) {
+        ans[lo] = v;
+    }
+    return ans;
+}
+
+std::mt19937_64 rng(
+    std::chrono::steady_clock::now().time_since_epoch().count()
+);
+
+template <class T>
+void shuffle(typename std::vector<T>::iterator first, typename std::vector<T>::iterator last) {
+    std::shuffle(first, last, rng);
+}
+---------------------------------------------------------------------------------*/
