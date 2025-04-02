6bffa29afc2a7c148ef4fc0c5b165ba1a847ec1e
Vegetog <2542856720@qq.com>
2025-04-02 15:33:37 +0800

    优化 Gawr Gura.cpp 中的宏定义，改进 size 函数实现，提升代码可读性

--- Gawr Gura.cpp
+++ Gawr Gura.cpp
@@ -1,11 +1,11 @@
-#include "bits/stdc++.h" 
+#include "bits/stdc++.h"
 using namespace std;
 #define x1 X1
 #define x2 X2
 #define y1 Y1
 #define y2 Y2
 #define int long long
-#define size(c) static_cast<int>((c).size())
+template <typename C> inline int size(const C& x) {return static_cast<int>(x.size());}
 
 using i64 = long long;
 using pii = pair<int, int>;
@@ -13,9 +13,9 @@ using pii = pair<int, int>;
 constexpr int B = 30, N = 2E6 + 10, mod = 998244353, inf32 = 1E9 + 10;
 constexpr i64 inf = 2E18 + 10;
 
-void Gura()
+void Gura() 
 {
-    
+
 }
 
 signed main() {
@@ -25,7 +25,7 @@ signed main() {
 
     int Gawr = 1;
     cin >> Gawr;
-    for(int _i = 1; _i <= Gawr; _i++) {
+    for (int _i = 1; _i <= Gawr; _i++) {
         Gura();
     }
 
