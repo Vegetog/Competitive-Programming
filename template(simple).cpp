77719694654de6b43eb13bd783e278f07683b000
Vegetog <2542856720@qq.com>
2025-04-17 14:17:19 +0800

    重构 template.cpp，移除宏定义，简化代码结构；新增 template.py 文件，添加 Gura 函数框架

--- template(simple).cpp
+++ template(simple).cpp
@@ -1,33 +1,20 @@
 #include "bits/stdc++.h"
-#define int long long
 
-using i128 = __int128;
+using i64 = __int64_t;
+using i128 = __int128_t;
 using pii = std::pair<int, int>;
 
-constexpr int N = 1E6 + 10, inf64 = 1E18 + 10;
-
-signed main() {
+int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
     int n;
     std::cin >> n;
-    auto p = std::vector<std::vector<int>>(N + 1);
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; ++i) {
         int x;
         std::cin >> x;
-        p[x].push_back(i);
+        std::cout << (x * x + (x * log(x))) << "\n";
     }
-
-    int ans = inf64;
-    for (int i = 1; i <= N; i++) {
-        if (p[i].size() <= 1) continue;
-        for (int j = 1; j < p[i].size(); j++) {
-            ans = std::min(ans, p[i][j] - p[i][j - 1] + 1);
-        }
-    }
-
-    std::cout << (ans == inf64 ? -1 : ans) << "\n";
 
     return 0;
 }
---
+++ template.py
@@ -0,0 +1,17 @@
+import sys
+from functools import lru_cache
+
+input = sys.stdin.readline  
+sys.setrecursionlimit(10 ** 8)  
+
+# @lru_cache(maxsize=None)
+
+def Gura():
+    pass
+
+
+if __name__ == '__main__':
+    Gawr = 1
+    Gawr = int(input())
+    for _i in range(Gawr):
+        Gura()
