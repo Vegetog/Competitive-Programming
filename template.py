1cc0beae1b2c064852106f51396b72ed087c444e
Vegetog <2542856720@qq.com>
2025-07-25 19:15:45 +0800

    重构模板代码，添加字符串分割函数，更新加法输出格式，删除冗余 KMP 测试文件

--- template.py
+++ template.py
@@ -1,10 +1,17 @@
 import sys
-from functools import lru_cache
+from functools import lru_cache # @lru_cache(maxsize=None)
 input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 8)
 
-# @lru_cache(maxsize=None)
+def str_div(s): # "aaabbbccccaaddd" -> ["aaa", "bbb", "ccccc", "aa", "ddd"]
+    from itertools import groupby
+    return [''.join(g) for _, g in groupby(s)]
+def str_div_sg(s): # "aaabbbccccaaddd" -> ["a", "b", "c", "a", "d"]
+    import re
+    return re.findall(r'(.)\1*', s)
+
+mod = 10 ** 9 + 7
 
 for _i in range(int(input())):
     a, b = map(int, input().split())
-    print('a + b = ',a + b)
+    print('a + b = ',(a + b) % mod)
