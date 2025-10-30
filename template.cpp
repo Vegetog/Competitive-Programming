2ed60cc2fb877f406fdf7de87eb5019ddb505620
Vegetog <2542856720@qq.com>
2025-10-30 23:10:51 +0800

    更新模板代码，添加调试宏定义，调整代码格式

--- template.cpp
+++ template.cpp
@@ -1,16 +1,24 @@
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
+#define int long long
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << "=" << (x) << endl
+#define debuga(a) cerr << #a << "=["; for (auto& v : a) cerr << v << " "; cerr << "]" << endl
+#else
+#define debug(x)
+#define debuga(a)
+#endif
 
 using i64 = long long;
-using pii = std::pair<int, int>;
+using pii = pair<int, int>;
 
 constexpr int N = 1e6 + 5, mod = 998244353;
 constexpr i64 inf = 2e18 + 5;
 
-void solve() 
+void solve()
 {
-    
+
 }
 
 signed main() {
