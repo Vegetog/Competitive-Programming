f0edd851870c017947f8b4eccbc8781473e7bf66
Vegetog <2542856720@qq.com>
2025-11-04 13:03:15 +0800

    更新模板代码，删除冗余的构建任务和调试宏定义，优化代码结构

--- template.cpp
+++ template.cpp
@@ -1,10 +1,13 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
 #define int long long
 
 #ifndef ONLINE_JUDGE
 #define debug(x) cerr << #x << "=" << (x) << endl
-#define debuga(a) cerr << #a << "=["; for (auto& v : a) cerr << v << " "; cerr << "]" << endl
+#define debuga(a)                       \
+    cerr << #a << "=[";                 \
+    for (auto& v : a) cerr << v << " "; \
+    cerr << "]" << endl
 #else
 #define debug(x)
 #define debuga(a)
@@ -12,13 +15,13 @@ using namespace std;
 
 using i64 = long long;
 using pii = pair<int, int>;
-
+               
 constexpr int N = 1e6 + 5, mod = 998244353;
 constexpr i64 inf = 2e18 + 5;
 
 void solve()
 {
-
+    // code here
 }
 
 signed main() {
